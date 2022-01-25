64b3df260cc77de3a12e90025776277c95830aee
Aaron Sando <Aaron@Charactercabinets.com.au>
2022-01-25 09:59:13 +0800

    last updates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,13 @@
 const router = require("express").Router();
 const fs = require("fs");
 const uuid = require('uuid');
-// API GET request to read and display db.json
-router.get("/notes", function(req, res) {
 
-  fs.readFile("./db/db.json", "utf8", function(err, data) {
-      
-    if (err) throw err;
-    res.json(JSON.parse(data));
-
-  });
-
-});
-
-// API POST request saves and writes note to db.json.
+// POST note
 router.post("/notes", function(req, res) {
 
   fs.readFile("./db/db.json", "utf8", function(err, data) {
     if (err) throw err;
-    // Parse and store db.json data to raw
     let raw = JSON.parse(data);
-    // push note
     raw.push({
       ...req.body,
       id: uuid.v4(),
@@ -36,21 +23,28 @@ router.post("/notes", function(req, res) {
   
 });
 
-// API DELETE request to delete note
-router.delete("/notes/:id", function(req, res) {
+// GET note
+router.get("/notes", function(req, res) {
 
-  // grab id and store 
+  fs.readFile("./db/db.json", "utf8", function(err, data) {
+      
+    if (err) throw err;
+    res.json(JSON.parse(data));
+
+  });
+
+});
+
+// DELETE note
+router.delete("/notes/:id", function(req, res) {
+ 
   let id = req.params.id;
 
   fs.readFile("./db/db.json", "utf8", function(err, data) {
     if (err) throw err;
-    // Parse and store db.json data to raw
     let raw = JSON.parse(data);
-    // Loop through raw
     for (let i = 0; i < raw.length; i++) {
-      // id matches id in raw
       if (id == raw[i].id) {
-        // If match, splice the indexed note out of raw 
         raw.splice(i,1);
 
         fs.writeFile("./db/db.json", JSON.stringify(raw), function(err) {
